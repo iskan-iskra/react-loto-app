e920874afed23f8172f0ec1dee7c44c6c7bee94e
Iskander <i.shushayev@mycar.kz>
2024-08-28 17:22:17 +0500

    fix vh for mobile brouwers

--- src/main.tsx
+++ src/main.tsx
@@ -3,4 +3,7 @@ import App from "./App.tsx";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+const vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty("--vh", `${vh}px`);
+
 createRoot(document.getElementById("root")!).render(<App />);
