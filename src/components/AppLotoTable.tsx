5783f6ab40d83dbcff6cff7aed061cd417ba514e
Iskander <i.shushayev@mycar.kz>
2024-08-28 17:05:54 +0500

    hot-fix app-loto-table cell at mobile

--- src/components/AppLotoTable.tsx
+++ src/components/AppLotoTable.tsx
@@ -7,12 +7,12 @@ type TiAppLotoTableProps = {
 
 export default function AppLotoTable({ lotoTable }: TiAppLotoTableProps) {
   return (
-    <Table bordered>
+    <Table bordered style={{ maxWidth: "100%" }}>
       <tbody>
         {lotoTable.map((el, index) => (
           <tr key={index}>
             {el.map((el, index) => (
-              <td className="text-center" key={index}>
+              <td className="text-center app-loto-table-cell" key={index}>
                 {el || "-"}
               </td>
             ))}
